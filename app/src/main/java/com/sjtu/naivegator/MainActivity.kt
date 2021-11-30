04782598a56f88c5171ef5f2ea9a2383dd526c89
floudk <a23187q@sina.cn>
2021-11-30 12:39:20 +0800

    add canteen positions

--- app/src/main/java/com/sjtu/naivegator/MainActivity.kt
+++ app/src/main/java/com/sjtu/naivegator/MainActivity.kt
@@ -3,9 +3,18 @@ package com.sjtu.naivegator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
+
+
+
+
+
