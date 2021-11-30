f6f93639c3480a80411f22121cc83fda2992e354
floudk <a23187q@sina.cn>
2021-11-30 13:59:18 +0800

    basic popup fragment xml and settings

--- app/src/main/java/com/sjtu/naivegator/MainActivity.kt
+++ app/src/main/java/com/sjtu/naivegator/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.sjtu.naivegator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.sjtu.naivegator.filter.FilterFragment
 
 
 class MainActivity : AppCompatActivity() {
 
+    var fragmentManager: FragmentManager? = null
+    var fragmentTransaction: FragmentTransaction? = null
+
+    var filterFragment : FilterFragment ?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragmentManager= supportFragmentManager;
+        fragmentTransaction = fragmentManager!!.beginTransaction()
+        filterFragment = FilterFragment()
+        invoke_filter()
+
     }
+
+
+    private fun invoke_filter(){
+        fragmentTransaction!!.add(R.id.content, filterFragment!!)
+        fragmentTransaction!!.commit()
+    }
+
 }
 
 
---
+++ app/src/main/java/com/sjtu/naivegator/filter/FilterFragment.kt
@@ -0,0 +1,36 @@
+package com.sjtu.naivegator.filter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sjtu.naivegator.R
+
+
+class FilterFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filter, container, false)
+    }
+
+    companion object {
+
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            FilterFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
