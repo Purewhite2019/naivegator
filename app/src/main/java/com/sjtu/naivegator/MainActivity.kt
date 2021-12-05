9efda4e7542bae840661a93b66880ce296c6e9da
Purewhite2019 <purewhite@sjtu.edu.cn>
2021-12-05 16:18:37 +0800

    Settings Finished. (Though with an ugly UI); Databases finished.

--- app/src/main/java/com/sjtu/naivegator/MainActivity.kt
+++ app/src/main/java/com/sjtu/naivegator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sjtu.naivegator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, ActivitySettings::class.java))
     }
 }
